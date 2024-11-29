95c86b1b8ac7c923a10a63606415b869658e542e
Matt <mpickle86@gmail.com>
2024-11-29 08:35:38 -0600

    fix button disabling

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
           <path fill-rule="evenodd" clip-rule="evenodd" d="M1.31134e-07 6.91672L16 6.91672V8.41672L0 8.41672L1.31134e-07 6.91672Z" fill="white"/>\
         </svg>\
       </button>',
-    prevArrow: '<button class="story__prev-button story__disabled">\
+    prevArrow: '<button class="story__prev-button story__disabled" disabled>\
         <svg width="18" height="15" viewBox="0 0 18 15" fill="none" xmlns="http://www.w3.org/2000/svg">\
           <path fill-rule="evenodd" clip-rule="evenodd" d="M3.06067 7.66672L9.197 1.53039L8.13634 0.469727L0.939346 7.66672L8.13634 14.8637L9.197 13.8031L3.06067 7.66672Z" fill="white"/>\
           <path fill-rule="evenodd" clip-rule="evenodd" d="M18 6.91672L2.00001 6.91672L2.00001 8.41672L18 8.41672V6.91672Z" fill="white"/>\
@@ -21,11 +21,11 @@ $(document).ready(function() {
   })
 
   $('.story__slider').on('afterChange', function(){
-    $('.story__prev-button, .story__next-button').attr("disabled", false).removeClass("story__disabled")
+    $('.story__prev-button, .story__next-button').prop("disabled", false).removeClass("story__disabled")
     if ($('.story__slide:first-child').hasClass("slick-active")) {
-      $('.story__prev-button').attr("disabled", true).addClass("story__disabled")
+      $('.story__prev-button').prop("disabled", true).addClass("story__disabled")
     } else if ($('.story__slide:last-child').hasClass("slick-active")) {
-      $('.story__next-button').attr("disabled", true).addClass("story__disabled")
+      $('.story__next-button').prop("disabled", true).addClass("story__disabled")
     }
   })
 })
